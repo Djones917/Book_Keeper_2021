d74b42b2a8c9beee50038d292cdfc300a843baac
Derek <nightwing917@hotmail.com>
2021-04-19 20:43:25 -0600

    added showModal function

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
+
+// Show Modal, Focus on Input
+function showModal() {
+    modal.classList.add('show-modal');
+    websiteNameEl.focus();
+}
+
+// Modal Event Listeners
