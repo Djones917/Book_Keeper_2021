2f18c58b224832eaa2705886aae2a73b39000c9f
Derek <nightwing917@hotmail.com>
2021-04-24 19:22:29 -0600

    added e prevent default to store book mark function

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
 
+// Handle Data From Form
 function storeBookmark(e) {
      console.log(e);
      preventDefault();
