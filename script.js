1a41a450ecd6967cc7976547bc83e828a0b8f8cb
Derek <nightwing917@hotmail.com>
2021-04-27 19:54:54 -0600

    adding validate function for reg expression for https websites

--- script.js
+++ script.js
@@ -17,6 +17,18 @@ modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
 
+// Validate Form
+function validate(nameValue, urlValue) {
+   const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+   const regex = new RegExp(expression);
+   if (urlValue.match(regex)) {
+      alert('Match');
+   }
+   if (!urlValue.match(regex)) {
+    alert('Please provide a valid web address!');
+ }
+}
+
 // Handle Data From Form
 function storeBookmark(e) {
     e.preventDefault();
