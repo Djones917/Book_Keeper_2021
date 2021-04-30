293a43197a174b6b2f8ec51737cfb4000685dbdd
Derek <nightwing917@hotmail.com>
2021-04-29 19:33:49 -0600

    finished form validation

--- script.js
+++ script.js
@@ -22,15 +22,16 @@ function validate(nameValue, urlValue) {
    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
    const regex = new RegExp(expression);
    if (!nameValue || !urlValue) {
-       alert('Please submit a valid website name and URL Address, both fields are required!');
-   }
-   if (urlValue.match(regex)) {
-      alert('Match');
-   }
+       alert('Please submit values for both fields!');
+       return false;
+   }  
    if (!urlValue.match(regex)) {
     alert('Please provide a valid web address!');
     return false;
  }
+ //Valid
+
+ return true;
 }
 
 // Handle Data From Form
@@ -42,7 +43,9 @@ function storeBookmark(e) {
         urlValue = `https://${urlValue}`;
     }
     console.log(nameValue, urlValue);  
-    validate(nameValue, urlValue);
+    if (!validate(nameValue, urlValue)) {
+       return false;
+    }
 }
 
 // Event Listeners
