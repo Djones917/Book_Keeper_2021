84522d130f5209ab730b5d9aa675a35a6f4833a4
Derek <nightwing917@hotmail.com>
2021-05-07 19:34:00 -0600

    fixed favicon issue

--- script.js
+++ script.js
@@ -43,25 +43,37 @@ function validate(nameValue, urlValue) {
 
 // Build Bookmarks DOM
 function buildBookmarks() {
-    // Build Items
+    // Remove all bookmark elements
+    bookmarksContainer.textContent = '';
+    // Build items
     bookmarks.forEach((bookmark) => {
-       const { name, url } = bookmark;
-       // Item
-       const item = document.createElement('div');
-       item.classList.add('item');
-       // Close Icon
-       const closeIcon = document.createElement('i');
-       closeIcon.classList.add('fas', 'fa-times');
-       closeIcon.setAttribute('title', 'Delete Bookmark');
-       closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
-       // Favicon / Link Container
-       const linkInfo = document.createElement('div');
-       linkInfo.classList.add('name');
-       // Favicon
-       const favicon = document.createElement('img');
-       favicon.setAttribute('');      
+      const { name, url } = bookmark;
+      // Item
+      const item = document.createElement('div');
+      item.classList.add('item');
+      // Close Icon
+      const closeIcon = document.createElement('i');
+      closeIcon.classList.add('fas', 'fa-times');
+      closeIcon.setAttribute('title', 'Delete Bookmark');
+      closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+      // Favicon / Link Container
+      const linkInfo = document.createElement('div');
+      linkInfo.classList.add('name');
+      // Favicon
+      const favicon = document.createElement('img');
+      favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+      favicon.setAttribute('alt', 'Favicon');
+      // Link
+      const link = document.createElement('a');
+      link.setAttribute('href', `${url}`);
+      link.setAttribute('target', '_blank');
+      link.textContent = name;
+      // Append to bookmarks container
+      linkInfo.append(favicon, link);
+      item.append(closeIcon, linkInfo);
+      bookmarksContainer.appendChild(item);
     });
-}
+  }
 
 
 // Fetch Bookmars
